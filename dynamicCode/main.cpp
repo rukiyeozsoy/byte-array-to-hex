5876deebb40a913ed0d082224e2e1a7990c3a302
rukiyeozsoy <ozsoy.rukiye@gmail.com>
2023-05-31 23:12:33 +0300

    first

---
+++ dynamicCode/main.cpp
@@ -0,0 +1,40 @@
+#include <QDebug>
+#include <QByteArray>
+#include <QDataStream>
+#include <QIODevice>
+
+//qint32 değerini bir QByteArray'ya dönüştürme
+void int32_Paketle(QByteArray& byteArray, qint32 value)
+{
+    QDataStream stream(&byteArray, QIODevice::WriteOnly);
+    stream << value;
+}
+
+//QByteArray türündeki veriyi hexadecimal string olarak temsil etme
+QString byteArrayToHex(const QByteArray& byteArray)
+{
+    QString hexString;
+    for (int i = 0; i < byteArray.size(); ++i) {
+        hexString += QString("%1").arg(static_cast<quint8>(byteArray[i]), 2, 16, QChar('0'));
+    }
+    return hexString;
+}
+ //qint32 türündeki verileri QByteArray'ya paketleme işlemi
+//paketlenmiş veriyi hexadecimal formatta konsola basma
+int main()
+{
+    QByteArray Buffer;
+
+    qint32 value1 = 3680;
+    qint32 value2 = 50;
+    qint32 value3 = -680;
+
+    int32_Paketle(Buffer, value1);
+    int32_Paketle(Buffer, value2);
+    int32_Paketle(Buffer, value3);
+
+    QString hexString = byteArrayToHex(Buffer);
+    qDebug() << hexString;
+
+    return 0;
+}
